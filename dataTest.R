407e45a5d80b6a4200a015b9faf1434add32a234
Kineticske <karinaestherarjonariano@gmail.com>
2022-03-24 11:55:36 -0500

    Importing dataset

--- dataTest.R
+++ dataTest.R
@@ -1,1 +1,4 @@
 #Data preprocessing
+#importing dataset
+
+dataset= read.csv("C:\\Users\\Credd\\OneDrive\\Documentos\\SISTEMAS\\PRACTICAS\\python\\Machine Learning\\Machine Learning A-Z (Codes and Datasets)\\Part 1 - Data Preprocessing\\Section 2 -------------------- Part 1 - Data Preprocessing --------------------\\R\\Data.csv", header=TRUE)
