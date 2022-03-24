95de9c2d46bc999035ee0c6e3dac069993eb6812
Kineticske <karinaestherarjonariano@gmail.com>
2022-03-24 12:11:59 -0500

    Encoding data 2

--- dataTest.R
+++ dataTest.R
@@ -19,3 +19,7 @@ dataset$Salary =ifelse(is.na(dataset$Salary),
 dataset$Country =factor(dataset$Country, 
                         levels=c('France', 'Spain', 'Germany'),
                         labels=c(1,2,3))
+
+dataset$Purchased =factor(dataset$Purchased, 
+                        levels=c('Yes', 'No'),
+                        labels=c(1,0))
